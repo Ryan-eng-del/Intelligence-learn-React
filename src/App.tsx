dfa072445889ffec6aa2b8e9bb995ed2c896e019
PlutoCRown <minecrafttmi@foxmail.com>
2022-07-16 14:01:33 +0800

    fix: 修复上次提交的路由递归错误

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Outlet, Navigate } from 'react-router-dom'
+import { Outlet } from 'react-router-dom'
 import './App.css'
 import { Footer } from './AppStyle'
 function App() {
@@ -7,8 +7,7 @@ function App() {
   // useMount(() => navigate('login'))
   return (
     <div className="App">
-      {/* 当Navigate挂载将重定向至login */}
-      <Navigate to={'login'}></Navigate>
+      
       <Outlet />
       <Footer>2022@Intelligence-Learining</Footer>
     </div>
