872bd452f5f2eb518cb10572e3cc5f356000e9d4
LionHAO <cch3291387738@163.com>
2022-09-04 09:27:29 +0800

    feat: 修完合并报错一切正常的节点

--- src/pages/StudentClassInfoPage/cpn-page/KnowledgePage/KnowledgePage.tsx
+++ src/pages/StudentClassInfoPage/cpn-page/KnowledgePage/KnowledgePage.tsx
@@ -24,7 +24,7 @@
 
 import React from 'react'
 import { KnowledgeListWrapper } from './KnowledgePageStyle'
-import { KnowledgeTree } from 'publicComponents/ClassInfoPage'
+import { KnowledgeTree } from 'components/ClassInfoPage/ClassInfoRoutePage/KnowledgePage/KnowledgeTree/KnowledgeTree'
 export const StudentKnowledgePage: React.FC = () => {
   return (
     <>
