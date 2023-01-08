3236a314a6071e857df34756f55b93f4f33bf99e
cyan <cyan0908@163.com>
2023-01-08 18:24:02 +0800

    fix: fix bug where exists on preview page

--- src/publicComponents/CreateQuestionPage/QuestionType/ShortAnswer/Preview.tsx
+++ src/publicComponents/CreateQuestionPage/QuestionType/ShortAnswer/Preview.tsx
@@ -2,22 +2,20 @@ import { Tag } from 'antd'
 import React from 'react'
 import { QuestionDataWithID } from 'server/fetchExam/types'
 import { str2DOM } from 'util/str2DOM'
-import { Network2Data } from './config'
 
 export const Preview: React.FC<{
   content: QuestionDataWithID
   No?: number
 }> = ({ content, No }) => {
-  const question = Network2Data(content)
   return (
     <>
       {No ? <h1>{No.toString()}.</h1> : <></>}
       <h2>题目</h2>
-      {str2DOM(question.content)}
-
+      {str2DOM(content.questionDescription)}
+      {content.rightAnswer == '1' ? <h1>正确</h1> : <h1>错误</h1>}
       <h2>解析</h2>
-      {str2DOM(question.footer.explanation)}
-      {question.footer.knowledge?.map((i: any) => (
+      {str2DOM(content.questionAnswerExplain)}
+      {content.points.map((i: any) => (
         <Tag color="red" key={i}>
           {i}
         </Tag>
