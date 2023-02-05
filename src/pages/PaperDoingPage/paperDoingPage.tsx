cfab864a7650d6b533a268ad5741b6a7700a3098
cyan <cyan0908@163.com>
2023-02-05 23:00:50 +0800

    feat(paperDoingPage): finish left and right layout

--- src/pages/PaperDoingPage/paperDoingPage.tsx
+++ src/pages/PaperDoingPage/paperDoingPage.tsx
@@ -1,11 +1,11 @@
 import { ClockCircleOutlined } from '@ant-design/icons'
 import { Button, Typography } from 'antd'
 import Countdown from 'antd/lib/statistic/Countdown'
-import React, { useMemo, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { submitPaper } from 'server/fetchExam'
 import { useShowQuestionForStudent } from 'server/fetchExam/TestPaper'
-import { QuestionType, StudentPaper } from 'server/fetchExam/types'
+import { StudentPaperItem } from 'server/fetchExam/types'
 import styled from 'styled-components'
 
 const LeftExamWrapper = styled.div`
@@ -32,6 +32,41 @@ const CenterExamWrapper = styled.div`
 
 const RightExamWrapper = styled.div`
   width: 280px;
+  padding: 15px;
+  .status-bar {
+    display: flex;
+    margin-bottom: 13px;
+  }
+
+  .already-answer {
+    display: flex;
+    align-items: center;
+    margin-right: 30px;
+    &::before {
+      content: '';
+      border-radius: 3px;
+      display: inline-block;
+      width: 20px;
+      height: 20px;
+      background-color: #0ea5e9;
+      margin-right: 10px;
+    }
+  }
+
+  .no-answer {
+    display: flex;
+    align-items: center;
+    &::before {
+      content: '';
+      border-radius: 3px;
+      display: inline-block;
+      width: 20px;
+      border: 1px solid #0284c7;
+      height: 20px;
+      background-color: #fff;
+      margin-right: 10px;
+    }
+  }
 `
 
 const LayoutWrapper = styled.div`
@@ -84,27 +119,64 @@ const HeaderWrapper = styled.div`
   }
 `
 
+const QuestionBlock = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: #0284c7;
+  cursor: pointer;
+  border: 1px solid #0284c7;
+`
+
+const QuestionTypeWrapperStatus = styled.div`
+  padding: 10px;
+`
+
+const QuestionStatusHeader = styled.div`
+  display: flex;
+  margin-bottom: 15px;
+`
 const PaperDoing: React.FC = () => {
   // 需要路由获取参数
   const { paperId } = useParams()
   const { data: PaperData, isLoading } = useShowQuestionForStudent(paperId!)
-  console.log(PaperData, paperId, 'data')
   const { mutate: submitExam } = submitPaper(paperId!)
-  const len = (data: StudentPaper) => {
-    if (data) {
-      const list = data.questionOfPaperVOS
-      return [
-        list.filter((i) => i.questionType == QuestionType.single).length,
-        list.filter((i) => i.questionType == QuestionType.multiple).length,
-        list.filter((i) => i.questionType == QuestionType.fillBlank).length,
-        list.filter((i) => i.questionType == QuestionType.shortAnswer).length,
-        list.filter((i) => i.questionType == QuestionType.judge).length
-      ]
-    } else {
-      return []
-    }
-  }
+
   const [open, setOpen] = useState(true)
+
+  const paperMap = useMemo(() => ['single', 'multiple', 'judge', 'fillBank', 'shortAnswer'], [])
+  const paperNameMap = useMemo(() => ['单选题', '多选题', '判断题', '填空题', '简答题'], [])
+
+  interface ExamPaper {
+    single: StudentPaperItem[]
+    multiple: StudentPaperItem[]
+    fillBank: StudentPaperItem[]
+    shortAnswer: StudentPaperItem[]
+    judge: StudentPaperItem[]
+  }
+
+  const examQuestionData: ExamPaper = useMemo(() => {
+    const init = {
+      single: [],
+      multiple: [],
+      fillBank: [],
+      shortAnswer: [],
+      judge: []
+    }
+    if (!PaperData) return init
+    return PaperData?.questionOfPaperVOS?.reduce<Record<keyof ExamPaper, StudentPaperItem[]>>((pre, now) => {
+      pre[paperMap[now.questionType] as keyof ExamPaper] =
+        pre[paperMap[now.questionType] as keyof ExamPaper].concat(now)
+      return pre
+    }, init)
+  }, [PaperData])
+
+  useEffect(() => {
+    console.log(examQuestionData, 'data')
+  }, [])
 
   const clickSubmit = () => {
     submitExam()
@@ -141,12 +213,14 @@ const PaperDoing: React.FC = () => {
           <Typography.Title level={4} className="paperName">
             {PaperData?.paperName}
           </Typography.Title>
+
           <CountDownWrapper>
             <ClockCircleOutlined />
             <span className="count-num">
               <Countdown title="Time remaining" value={deadline} format="HH:mm:ss" />
             </span>
           </CountDownWrapper>
+
           <InfoWrapper>
             <div className="info-item">姓名：{PaperData?.studentName}</div>
             <div className="info-item">数量：{PaperData?.questionOfPaperVOS?.length}</div>
@@ -158,7 +232,39 @@ const PaperDoing: React.FC = () => {
         <CenterExamWrapper>
           <div className="son">1</div>
         </CenterExamWrapper>
-        <RightExamWrapper></RightExamWrapper>
+        <RightExamWrapper>
+          <div className="status-bar">
+            <div className="already-answer">已经作答</div>
+            <div className="no-answer">未作答</div>
+          </div>
+
+          <div>
+            {Object.keys(examQuestionData)?.map((k) => {
+              return examQuestionData[k as keyof ExamPaper]?.map((exam, index) => {
+                return (
+                  <QuestionTypeWrapperStatus key={index}>
+                    <QuestionStatusHeader>
+                      <Typography.Title level={5}>
+                        {paperNameMap[exam.questionType]}
+                        <span style={{ padding: '0 3px' }}></span>
+                        <span>
+                          {`(${examQuestionData[k as keyof ExamPaper].reduce((pre, now) => {
+                            return pre + (now.questionScore || 0)
+                          }, 0)}分)`}
+                        </span>
+                      </Typography.Title>
+                    </QuestionStatusHeader>
+                    <div>
+                      {examQuestionData[k as keyof ExamPaper]?.map((_, index) => {
+                        return <QuestionBlock key={index}>{index + 1}</QuestionBlock>
+                      })}
+                    </div>
+                  </QuestionTypeWrapperStatus>
+                )
+              })
+            })}
+          </div>
+        </RightExamWrapper>
         {/* <Header
             style={{
               position: 'sticky',
