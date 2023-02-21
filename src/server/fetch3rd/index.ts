804d2657b79212a0fd6ea034689f893b489ec1e1
LionHAO <cch3291387738@163.com>
2023-02-21 22:04:18 +0800

    fix: 个性设置项目贡献者头像显示bug

--- src/server/fetch3rd/index.ts
+++ src/server/fetch3rd/index.ts
@@ -1,15 +1,11 @@
 import { useQuery } from '@tanstack/react-query'
-import AXIOS from '../request/request'
+import axios from 'axios'
 
-export const client = new AXIOS({
-  baseURL: 'https://api.github.com',
-  timeout: 100000
-})
+
 
 export const useGetContributor = () => {
   return useQuery(['Contributor'], async () => {
-    return client.get({
-      url: '/repos/Ryan-eng-del/intelligence-learn/contributors'
-    })
+    return axios.get('https://api.github.com/repos/Ryan-eng-del/intelligence-learn/contributors'
+    )
   })
 }
