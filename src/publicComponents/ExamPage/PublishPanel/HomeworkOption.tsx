e4f9ae6e16f88ec4acfb96b8d06a94928f88d0b9
Eureka <euraka77035@gmail.com>
2023-04-10 20:27:07 +0800

    feat: commit a test

--- src/publicComponents/ExamPage/PublishPanel/HomeworkOption.tsx
+++ src/publicComponents/ExamPage/PublishPanel/HomeworkOption.tsx
@@ -1,5 +1,4 @@
-import { Switch } from 'antd'
-import Select from 'rc-select'
+import { Select, Switch } from 'antd'
 import React from 'react'
 const { Option } = Select
 
@@ -10,47 +9,56 @@ export const HomeworkOption: React.FC<{
   setIsAllowRedo: any
 }> = ({ setPublishOption, publishOption, isAllowRedo, setIsAllowRedo }) => {
   return (
-    <div>
-      <>
-        <span className="bl"> 允许重做:</span>
-        <Switch
-          checked={publishOption.isAllowMakeUp}
-          onChange={(value) => {
-            setIsAllowRedo(value), setPublishOption({ ...publishOption, isAllowMakeUp: value ? 1 : 0 })
-          }}
-        />
-        <br></br>
-        <span className={isAllowRedo ? 'bl' : 'gray'}> 取最高分:</span>
-        <Switch
-          checked={publishOption.isGetHighScore && isAllowRedo}
-          disabled={!isAllowRedo}
-          onChange={(value) => {
-            setPublishOption({ ...publishOption, isGetHighScore: value ? 1 : 0 })
-          }}
-        />
-        <br></br>
-        <span className={isAllowRedo ? 'bl' : 'gray'}> 是否多选题未选全给一半分:</span>
-        <Switch
-          checked={publishOption.isMultiHalfScore && isAllowRedo}
-          disabled={!isAllowRedo}
-          onChange={(value) => {
-            setPublishOption({ ...publishOption, isMultiHalfScore: value ? 1 : 0 })
-          }}
-        />
-        <br></br>
-        <span className={isAllowRedo ? 'bl' : 'gray'}> 允许重做几次:</span>
-        <Select
-          disabled={!isAllowRedo}
-          defaultValue={1}
-          onChange={(value: number) => {
-            setPublishOption({ ...publishOption, remakeTime: value })
-          }}
-        >
-          <Option value={1}>1</Option>
-          <Option value={2}>2</Option>
-          <Option value={3}>3</Option>
-        </Select>
-      </>
-    </div>
+    <>
+      <div>
+        <div>
+          <span className="bl"> 允许重做:</span>
+          <Switch
+            checked={publishOption.isAllowMakeUp}
+            onChange={(value) => {
+              setIsAllowRedo(value), setPublishOption({ ...publishOption, isAllowMakeUp: value ? 1 : 0 })
+            }}
+          />
+        </div>
+        <br />
+        <div style={{ display: isAllowRedo ? 'block' : 'none' }}>
+          <div>
+            <span className="bl"> 允许重做几次:</span>
+            <Select
+              disabled={!isAllowRedo}
+              onChange={(value: number) => {
+                setPublishOption({ ...publishOption, remakeTime: value })
+              }}
+            >
+              <Option value={1}>1</Option>
+              <Option value={2}>2</Option>
+              <Option value={3}>3</Option>
+            </Select>
+          </div>
+          <br />
+          <div>
+            <span className="bl"> 取最高分:</span>
+            <Switch
+              checked={publishOption.isGetHighScore && isAllowRedo}
+              disabled={!isAllowRedo}
+              onChange={(value) => {
+                setPublishOption({ ...publishOption, isGetHighScore: value ? 1 : 0 })
+              }}
+            />
+          </div>
+          <br />
+          <div>
+            <span className="bl"> 是否多选题未选全给一半分:</span>
+            <Switch
+              checked={publishOption.isMultiHalfScore && isAllowRedo}
+              disabled={!isAllowRedo}
+              onChange={(value) => {
+                setPublishOption({ ...publishOption, isMultiHalfScore: value ? 1 : 0 })
+              }}
+            />
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
